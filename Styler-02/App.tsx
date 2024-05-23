535ac911a949cfe97f38b1ad6016c8676b5ec861
Ashutosh <ashutosh.verma@sysfore.com>
2024-05-23 17:40:28 +0530

    Elevated Cards

--- Styler-02/App.tsx
+++ Styler-02/App.tsx
@@ -1,12 +1,14 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import FlatCard from './components/FlatCard'
+import ElevatedCard from './components/ElevatedCard'
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
         <FlatCard/>
+        <ElevatedCard/>
       </ScrollView>
     </SafeAreaView>
   )
